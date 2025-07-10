8a7d0a1804bed31ad49763ef659429227dc1daa1
ayoubban2003 <ayoubbomrane@gmail.com>
2025-07-10 17:30:45 +0100

    Defined user entity, and necessary controllers for sign in and sign up

--- src/main/java/com/medianet/fishingCompetiton/DTOs/SignUpDTO.java
+++ src/main/java/com/medianet/fishingCompetiton/DTOs/SignUpDTO.java
@@ -1,4 +1,4 @@
-package com.medianet.fishingCompetiton.dtos;
+package com.medianet.fishingCompetiton.DTOs;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
